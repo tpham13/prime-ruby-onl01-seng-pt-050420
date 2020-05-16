a1cda6b737ff69198f40c49c64840a421115cead
Thanh Pham <tpham0123@gmail.com>
2020-05-16 03:07:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,10 @@ def prime?(number)
   if number <= 1
     return false 
   else 
-    range = (2..candidate-1).to_a 
+    range = (2..number-1).to_a 
     
     range.each do |num|
-      if candidate % num == 0 
+      if number % num == 0 
         return false 
       end 
     end 

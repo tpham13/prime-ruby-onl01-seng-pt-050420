5a0fa0d92a9b6874fb65eb4315c46085f3f61bda
Thanh Pham <tpham0123@gmail.com>
2020-05-16 03:09:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false 
+  else 
+    range = (2..number-1).to_a 
+    
+    range.each do |num|
+      if number % num == 0 
+        return false 
+      end 
+  end 
+    true 
+  end 
+
+end
+  
+   

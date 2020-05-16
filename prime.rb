154302708864c30eb50383ff456918344c741df6
Thanh Pham <tpham0123@gmail.com>
2020-05-16 03:07:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,16 @@
 # Add  code here!
-def prime?()
-  i = 0 
-  prime.each do |n|
+def prime?(number)
+  if number <= 1
+    return false 
+  else 
+    range = (2..candidate-1).to_a 
+    
+    range.each do |num|
+      if candidate % num == 0 
+        return false 
+      end 
+    end 
+    true 
+end 
+  
    
-end
